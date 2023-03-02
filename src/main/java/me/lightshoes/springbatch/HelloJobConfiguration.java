5cc6af1f3b0ab1e2708ba1602b48689ecc9030cb
ShinKwang <sklee0206@naver.com>
2023-03-02 17:55:39 +0900

    Spring Batch git start

---
+++ src/main/java/me/lightshoes/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,36 @@
+package me.lightshoes.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class HelloJobConfiguration {
+
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello Spring Batch");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
